e4d9496d397918c76f635c054f4f1481cae06653
menapedreira <mena.pedreira@gmail.com>
2018-10-01 13:42:50 +0100

    Add files via upload

---
+++ PROJ_CG_1/js/lamp.js
@@ -0,0 +1,49 @@
+/*----------LAMP-----------------------*/
+
+function addLampTop(obj, x, y, z) {
+    'use strict';
+    geometry = new THREE.ConeGeometry(4, 8, 32);
+    mesh = new THREE.Mesh(geometry, material);
+    mesh.position.set(x, y+4, z);
+    mesh.rotation.x = Math.PI;
+    obj.add(mesh);
+}
+
+
+function addLampBase(obj, x, y, z) {
+    'use strict';
+    geometry = new THREE.CylinderGeometry(5, 5, 2, 32);
+    mesh = new THREE.Mesh(geometry, material);
+    mesh.position.set(x, y, z);
+    obj.add(mesh);
+}
+
+function addLampStand(obj, x, y, z) {
+    'use strict';
+    geometry = new THREE.CylinderGeometry(0.5, 0.5, 24, 22);
+    mesh = new THREE.Mesh(geometry, material);
+    mesh.position.set(x, y+12, z);
+    obj.add(mesh);
+}
+
+function createLamp(x, y, z) {
+    'use strict';
+    
+    lamp = new THREE.Object3D();
+    
+    material = new THREE.MeshBasicMaterial({ color: 0xD60F0F, wireframe: true });
+   
+    addLampBase(lamp,0,0,0);
+    addLampStand(lamp,0,1,0);
+    addLampTop(lamp,0,24,0);
+
+    lamp.name = "lamp";
+    
+    scene.add(lamp);
+    
+    lamp.position.x = x;
+    lamp.position.y = y;
+    lamp.position.z = z;
+}
+
+/*----------LAMP-----------------------*/
